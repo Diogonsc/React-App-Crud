329158509fa732ab6eb3b943bf86ddd3641d157c
Diogonsc <diogon21@yahoo.com.br>
2024-07-24 21:48:11 -0300

    fix: add tratamento de erros com notificações de usuário, add lib react-toastift

--- src/services/createClient.ts
+++ src/services/createClient.ts
@@ -1,13 +1,18 @@
 import { FormData } from "../components/FormComponent";
 import { getApi } from "../utils/api";
+import { toast } from "react-toastify";
 
 const api = getApi();
 
 export const createClient = async (data: FormData): Promise<void> => {
   try {
     await api.post("/api/clients", data);
+    toast.success("Cliente criado com sucesso!");
   } catch (error) {
     console.error("Erro ao criar cliente:", error);
+    toast.error("Erro ao criar cliente. Por favor, tente novamente.");
     throw error;
   }
 };
+
+
--- src/services/deleteClient.ts
+++ src/services/deleteClient.ts
@@ -1,12 +1,15 @@
 import { getApi } from "../utils/api";
+import { toast } from "react-toastify";
 
 const api = getApi();
 
 export const deleteClient = async (id: number): Promise<void> => {
   try {
     await api.delete(`/api/clients/${id}`);
+    toast.success("Cliente excluído com sucesso!");
   } catch (error) {
     console.error("Erro ao excluir cliente:", error);
+    toast.error("Erro ao excluir cliente. Por favor, tente novamente.");
     throw error;
   }
 };
